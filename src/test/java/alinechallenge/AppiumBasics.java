de14e6b48d6f2a230c0e055e86b9d2eaeb18c069
Alinebarreto <aline.michelle.barreto@gmail.com>
2023-12-17 19:43:27 +0000

    Test cases included

--- src/test/java/alinechallenge/AppiumBasics.java
+++ src/test/java/alinechallenge/AppiumBasics.java
@@ -21,51 +21,99 @@ public class AppiumBasics extends BaseTest{
 	{
 		
 
-		//Native View - open ok
-		//driver.findElement(By.className("android.widget.TextView")).click();
-		//driver.findElement(By.xpath("//android.widget.TextView[@content-desc='chainedView']")).click();	
-		//driver.findElement(By.xpath("//android.widget.TextView[@text='Back']")).click();
-		
-		//Slider - open ok
-		//driver.findElement(By.className("android.widget.TextView")).click();
-		//driver.findElement(By.xpath("//android.widget.TextView[@content-desc='slider1']")).click();
-		//driver.findElement(By.xpath("//android.widget.TextView[@text='Back']")).click();
-		
-		//Vertical Swiping - open ok
-		//driver.findElement(By.className("android.widget.TextView")).click();
-		//driver.findElement(By.xpath("//android.widget.TextView[@content-desc='verticalSwipe']")).click();
-		//driver.findElement(By.xpath("//android.widget.TextView[@text='Back']")).click();
-		
-		//Drag & Drop - open ok
-		//driver.findElement(By.className("android.widget.TextView")).click();
-		//driver.findElement(By.xpath("//android.widget.TextView[@content-desc='dragAndDrop']")).click();
-		//driver.findElement(By.xpath("//android.widget.TextView[@text='Back']")).click();
-		
-		//Double Tap - open ok
-		//driver.findElement(By.className("android.widget.TextView")).click();
-		//driver.findElement(By.xpath("//android.widget.TextView[@content-desc='doubleTap']")).click();
-		//driver.findElement(By.xpath("//android.widget.TextView[@text='Back']")).click();
-		
-		//Long Press - open ok
-		//driver.findElement(By.className("android.widget.TextView")).click();
-		//driver.findElement(By.xpath("//android.widget.TextView[@content-desc='longPress']")).click();
-		//driver.findElement(By.xpath("//android.widget.TextView[@text='Long Press Me']")).click();
 		
 		
-		//driver.findElement(By.xpath("//android.widget.TextView[@text='Back']")).click();
-		
-		//Photo View - open ok
-		//driver.findElement(By.className("android.widget.TextView")).click();
-		//driver.findElement(By.xpath("//android.widget.TextView[@content-desc='photoView']")).click();
-		//driver.findElement(By.xpath("//android.widget.TextView[@text='Back']")).click();
-		
-		//Web View - open ok
-		//driver.findElement(By.className("android.widget.TextView")).click();
-		//driver.findElement(By.xpath("//android.widget.TextView[@content-desc='webView']")).click();
-		//driver.findElement(By.xpath("//android.widget.TextView[@text='Back']")).click();
-		
+			//tap In Middle Of Screen
+				//Scenario: User tap in the middle of the screen
+		    		//Given I login to vodqa application using valid credentials
+		    		//When I tap in the middle of the screen
+		    		//Then I am able to move from "Samples List" page to next page
 
 		
+		//Native View - 
+		driver.findElement(By.className("android.widget.TextView")).click();
+		driver.findElement(By.xpath("//android.widget.TextView[@content-desc='chainedView']")).click();	
+		
+		driver.findElement(By.xpath("//android.widget.TextView[@text='Back']")).click();
+		
+		//Slider - 
+		driver.findElement(By.className("android.widget.TextView")).click();
+		driver.findElement(By.xpath("//android.widget.TextView[@content-desc='slider1']")).click();
+		
+		driver.findElement(By.xpath("//android.widget.TextView[@text='Back']")).click();
+		
+		//Vertical Swiping - 
+		//Scroll
+			//Scenario: Validating scroll functionality using 2 points
+				//Given I logged to vodqa application using valid credentials
+		    	//When I scroll from one to another element point on the vertical swiping screen
+		    	//Then Element text "Appium" should be visible
+		
+		driver.findElement(By.className("android.widget.TextView")).click();
+		driver.findElement(By.xpath("//android.widget.TextView[@content-desc='verticalSwipe']")).click();
+		
+		driver.findElement(By.xpath("//android.widget.TextView[@text='Back']")).click();
+		
+		
+		
+		//Drag & Drop -
+		  //Scenario: Validate that user is able to drag and drop
+		    //Given I login to vodqa application using valid credentials
+		    //When I drag the circle object to the drop target
+		    //Then I am able to view "Circle dropped" message
+
+		driver.findElement(By.className("android.widget.TextView")).click();
+		driver.findElement(By.xpath("//android.widget.TextView[@content-desc='dragAndDrop']")).click();
+		
+		driver.findElement(By.xpath("//android.widget.TextView[@text='Back']")).click();
+		
+		//Double Tap -
+			//Scenario: Validate that user is able to double tap on an element
+				//Given I login to vodqa application using valid credentials
+				//When I double click on 'Double Tap Me'
+		    	//Then I should be able see the message: 'Double tap successful!'
+		
+		driver.findElement(By.className("android.widget.TextView")).click();
+		driver.findElement(By.xpath("//android.widget.TextView[@content-desc='doubleTap']")).click();
+		
+		driver.findElement(By.xpath("//android.widget.TextView[@text='Back']")).click();
+		
+		//Long Press - 
+			//Scenario: Validate that user is able to long press on an element
+		    	//Given I login to vodqa application using valid credentials
+				//When I long press on element
+		    	//Then long pressed popup 'you pressed me hard :P' should be visible
+
+		
+		driver.findElement(By.className("android.widget.TextView")).click();
+		driver.findElement(By.xpath("//android.widget.TextView[@content-desc='longPress']")).click();
+		driver.findElement(By.xpath("//android.widget.TextView[@text='Long Press Me']")).click();
+	
+		driver.findElement(By.xpath("//android.widget.TextView[@text='Back']")).click();
+		
+		//Photo View - open ok
+		  	//Scenario: Validate that user is able to pinch and zoom on particular element
+				//Given I login to vodqa application using valid credentials
+				//Then I should be able to pinch and zoom in on an element
+				//And I should be able to pinch and zoom out on an element
+
+		driver.findElement(By.className("android.widget.TextView")).click();
+		driver.findElement(By.xpath("//android.widget.TextView[@content-desc='photoView']")).click();
+		
+		driver.findElement(By.xpath("//android.widget.TextView[@text='Back']")).click();
+		
+		//Web View - open ok
+		  	//Scenario: Validate context switching between native and web view context
+				//Given I login to vodqa application using valid credentials
+				//Then I am able to view hacker news login button inside webView section
+		    	//And I am able to view section header by navigating inside native view section
+
+		driver.findElement(By.className("android.widget.TextView")).click();
+		driver.findElement(By.xpath("//android.widget.TextView[@content-desc='webView']")).click();
+		
+		driver.findElement(By.xpath("//android.widget.TextView[@text='Back']")).click();
+		
+	
 		
 		
 	}
